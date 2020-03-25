fd24b54a5ea20b5534c06d9cfe7598173ce4e7d8
Johan Vandegriff <johan@vandymail.com>
2020-03-25 05:51:10 -0400

    basic gallery layout

--- routes.py
+++ routes.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 from flask import Flask, request, render_template, url_for
+import json, os
 
 app = Flask(__name__)
+# gallery_JSON = open("gallery_images.json").read()
 
-@app.route("/")
-def main():
-    return render_template("index.html", text="Hello, World!")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gallery_json')
+def gallery_json():
+    images = os.listdir("static/images")
+    images_json = [{"id": i+1, "url":"images/"+img} for i,img in enumerate(images)]
+    data = {
+        "album": {
+            "name": "VotePho.me"
+        },
+        "photos": images_json
+    }
+    # print(json.dumps(data, indent=4))
+    return json.dumps(data)
+    # return json.dumps(gallery_JSON)
 
 if __name__ == "__main__":
     app.run()
---
+++ static/js/gallery.js
@@ -0,0 +1,218 @@
+// $(function() {
+	var $header = $("#header");
+	var $frame = $('#frame');
+	var $slider = $('#slider');
+	var $themes = $("#themes");
+	var $nav = $('#nav');
+	var $image = $frame.find('.photo');
+	var $caption = $frame.children('.photo-caption');
+	var $thumbnails = $slider.children('#thumbnails');
+	var $slider_btn = $slider.children('span');
+	var thumbnails_width = 0;
+	var $edge;
+	var $selected;
+	var currPhoto;
+
+	// Get gallery json data & initialize page
+	$.getJSON('/gallery_json', function(data) {
+		// data = JSON.parse(data);
+		$header.children('.album').text(data.album.name);
+		photos = data.photos;
+		thumbnails = "";
+		
+		// Create thumbnails
+		for(var i=0; i < data.photos.length; i++){
+			photo = data.photos[i];
+			if (photo.thumb_url == undefined) { //if no thumbnail was given, use the original image
+				photo.thumb_url = photo.url;
+			}
+			thumbnails += "<li><img data-index=" + photo.id + " src='/static/" + photo.thumb_url +
+										"' alt='" + photo.title + "' title='" + photo.title + "' /></li>";
+		}
+		$thumbnails.html(thumbnails);
+
+		// Select first photo and thumbnail by default
+		currPhoto = parseInt(photos[0].id, 10);
+		setImage(currPhoto);
+		firstThumbnail = $thumbnails.children(":first");
+		selectThumbnail(firstThumbnail);
+
+		// Calculate and initiate slider if thumbnails overflow
+		var first = firstThumbnail.children("img"); // check if loaded
+		if (first[0].complete) {
+			initSlider();
+		} else {
+			// init slider after thumbnail images load
+			first.load(initSlider);
+		}
+
+		// Make page visible
+		$('body').css("opacity", "1");
+	});
+
+	// Select thumbnail & image on click
+	$thumbnails.on('click', 'img', function(event){
+		$this = $(this);
+		var photoID = parseInt($this.data('index'), 10);
+		setImage(photoID);
+		selectThumbnail($this.parent('li'), photoID);
+		currPhoto = photoID;
+	});
+
+	// Display next or previous photo on click
+	$nav.on('click', 'button', function(event){
+		var dir = this.className;
+		if (dir.indexOf('prev') >= 0 && currPhoto > 1) {
+			currPhoto--;
+			prevThumbnail();
+		} else if (dir.indexOf('next') >= 0 && currPhoto < photos.length) {
+			currPhoto++;
+			nextThumbnail();
+		}
+		setImage(currPhoto);
+	});
+
+	// Display slider based on window size
+	$(window).on('resize', function(){
+		sliderDisplay();
+		$thumbnails.children().css("display", "inline-block");
+		$edge = $thumbnails.children(":first");
+		displaySliderBtn('prev', false);
+		displaySliderBtn('next', true);
+	});
+
+
+	// Display prev or next thumbnail in slider
+	$slider.on('click', 'span', function(event){
+		var dir = event.target.className;
+		var $first = $thumbnails.children(":first");
+		var last_pos = getLastPos();
+		
+		if (dir.indexOf('prev') >= 0 && $first.css('display') == "none") {
+			$prev = $edge.prev();
+			if ($prev.length > 0) {$edge = $prev;} // update if prev exist
+			$edge.css('display', 'inline-block');
+
+			// toggle slider buttons
+			if ($first[0] === $edge[0]) {displaySliderBtn('prev', false);}
+			displaySliderBtn('next', true);
+
+		} else if(dir.indexOf('next') >= 0 && last_pos > 0) {
+			$edge.css('display', 'none');
+			$edge = $edge.next();
+
+			// toggle slider buttons
+			displaySliderBtn('prev', true);
+			if (getLastPos() === 0) {displaySliderBtn('next', false);}
+		}
+	});
+
+	// Toggle gallery theme styles
+	$themes.on('click', 'span', function(event){
+		$this = $(this);
+		theme = $this.data('theme');
+
+		if (!$this.hasClass('active')) {
+			if(theme == "another") {
+				$('link[href="/static/css/original.css"]').attr({href: "/static/css/another.css"});
+				$nav.appendTo("#frame");
+				$caption.appendTo(".photo-wrapper");
+			} else {
+				$('link[href="/static/css/another.css"]').attr({href: "/static/css/original.css"});
+				$nav.appendTo("#header");
+				$caption.appendTo("#frame");
+			}
+
+			$themes.children('.active').removeClass('active');
+			$this.addClass('active');
+		}
+
+	});
+
+	function setImage(photoID) {
+		if (photoID > 0 && photoID <= photos.length) {
+			var photo = photos[photoID - 1];
+			$image.attr('src', '/static/' + photo.url);
+			var title = "";
+			if (photo.title != undefined) {
+				title = photo.title;
+			}
+			$caption.children('.title').text(title);
+			var text = "";
+			if (photo.date != undefined && photo.location != undefined) {
+				text = "Taken on " + photo.date + " in " + photo.location;
+			}
+			$caption.children(".data").text(text);
+		}
+	}
+
+	function selectThumbnail(thumbnail) {
+		if ($selected) {$selected.removeClass('selected');}
+		$selected = $(thumbnail); // update $selected
+		$selected.addClass('selected');
+	}
+
+	function prevThumbnail() {
+		$selected.removeClass('selected');
+		$selected = $selected.prev(); // update $selected
+		$selected.addClass('selected');
+	}
+
+	function nextThumbnail() {
+		$selected.removeClass('selected');
+		$selected = $selected.next(); // update $selected
+		$selected.addClass('selected');
+	}
+
+	function initSlider(){
+		$thumbnails.children().each(function(){thumbnails_width += $(this).outerWidth(true);});
+		$edge = $thumbnails.children(":first");
+		sliderDisplay();
+		displaySliderBtn('prev', false);
+	}
+
+	function sliderDisplay() {
+		slider_width = $slider.innerWidth();
+		if (slider_width < thumbnails_width && $slider_btn.css('display') == "none") {
+			$slider_btn.css('display', "block");
+		} else if(slider_width >= thumbnails_width && $slider_btn.css('display') == "block"){
+			$slider_btn.css('display', "none");
+		}
+	}
+
+	function displaySliderBtn(dir, display) {
+		$btn = $slider.children('.' + dir);
+		if (display) {
+			$btn.removeClass('disable');
+		} else {
+			$btn.addClass('disable');
+		}
+	}
+
+	function getLastPos() {
+		var $last = $thumbnails.children(":last");
+		return $last.position().top; // get position of last thumbnail for overflow check
+	}
+
+// });
+
+$(document).keydown(function(e) {
+	switch(e.which) {
+		case 37: // left
+		prevThumbnail();
+		break;
+
+		case 38: // up
+		break;
+
+		case 39: // right
+		nextThumbnail();
+		break;
+
+		case 40: // down
+		break;
+
+		default: return; // exit this handler for other keys
+	}
+	e.preventDefault(); // prevent the default action (scroll / move caret)
+});
